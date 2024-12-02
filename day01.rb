0760890158a19b962f27976635e8b52b4f92328f
Johnny <johnnydongelmans@gmail.com>
2024-12-02 18:36:00 +0100

    Day 01 complete :)

---
+++ day01.rb
@@ -0,0 +1,14 @@
+inputs = File.readlines('inputs/day01.txt').map { |line| line.split('   ').map(&:to_i) }
+transposed_inputs = inputs.transpose.each(&:sort!)
+
+result1 = transposed_inputs[0].zip(transposed_inputs[1]).sum do |i, j|
+  (i - j).abs
+end
+
+puts "Result 1: #{result1}"
+
+result2 = inputs.sum do |i, _|
+  i * transposed_inputs[1].count(i)
+end
+
+puts "Result 2: #{result2}"
