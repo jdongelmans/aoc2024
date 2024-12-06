f4314c251d8be9b6439f5b78ada6454677bce2bd
Johnny <johnnydongelmans@gmail.com>
2024-12-06 19:07:36 +0100

    Some speed improvements. Now part 2 runs in ~7 seconds :)

--- day06.rb
+++ day06.rb
@@ -19,7 +19,7 @@ DIRECTIONS = [
 ].freeze
 
 def walk_matrix(starting_matrix)
-  matrix = Marshal.load(Marshal.dump(starting_matrix))
+  matrix = starting_matrix.map(&:dup)
   current_position = find_starting_position(matrix)
   direction_idx = 0
 
@@ -54,28 +54,36 @@ end
 # ======================================================================
 
 AdventOfCode2024.run(6, 2) do
+  matrix_height = starting_matrix.size
+  matrix_width = starting_matrix[0].size
   visited_positions = walk_matrix(starting_matrix)
   starting_position = find_starting_position(starting_matrix)
   possible_obstructions = 0
 
   visited_positions.each do |x, y|
-    matrix = Marshal.load(Marshal.dump(starting_matrix))
+    matrix = starting_matrix.map(&:dup)
     current_position = starting_position
 
     next if current_position == [x, y]
 
     direction_idx = 0
     matrix[y][x] = '#'
-    visited_states = []
+    visited_states = Set.new
 
     loop do
-      current_state = "#{current_position[1]},#{current_position[0]}:#{direction_idx}"
+      current_state = (current_position[1]) | (current_position[0] << 8) | (direction_idx << 16)
+
+      if visited_states.include?(current_state)
+        possible_obstructions += 1
+
+        break
+      end
 
       dx, dy = DIRECTIONS[direction_idx]
       next_position = [current_position[0] + dx, current_position[1] + dy]
 
       if next_position[0].negative? || next_position[1].negative? ||
-         next_position[0] >= matrix[0].size || next_position[1] >= matrix.size
+         next_position[0] >= matrix_width || next_position[1] >= matrix_height
 
         break
       end
@@ -83,12 +91,6 @@ AdventOfCode2024.run(6, 2) do
       if matrix[next_position[1]][next_position[0]] == '#'
         direction_idx = (direction_idx + 1) % DIRECTIONS.size
       else
-        if visited_states.include?(current_state)
-          possible_obstructions += 1
-
-          break
-        end
-
         visited_states << current_state
         current_position = next_position
       end
