9b545faad377266595cfd8f02fee934cd9889526
Johnny <johnnydongelmans@gmail.com>
2024-12-08 00:43:44 +0100

    Day 7 part 2 complete, although slow. But it's late, whatever :D

--- day07.rb
+++ day07.rb
@@ -1,20 +1,19 @@
 require_relative 'advent_of_code_2024'
 
 inputs = File.readlines('inputs/day07.txt')
+input = inputs.map do |line|
+  result, numbers = line.split(':')
+  [result.to_i, numbers.split.map(&:to_i)]
+end
 
-POSSIBLE_OPERATORS = %i[+ *].freeze
+POSSIBLE_OPERATORS_PART1 = %i[+ *].freeze
 
 AdventOfCode2024.run(7, 1) do
-  input = inputs.map do |line|
-    result, numbers = line.split(':')
-    [result.to_i, numbers.split.map(&:to_i)]
-  end
-
   input.map do |result, numbers|
-    result if POSSIBLE_OPERATORS.repeated_permutation(numbers.size - 1).any? do |operators|
+    result if POSSIBLE_OPERATORS_PART1.repeated_permutation(numbers.size - 1).any? do |operators|
       total = numbers[0]
       (numbers.size - 1).times do |i|
-        total = total.send(operators[i], numbers[i+1])
+        total = total.send(operators[i], numbers[i + 1])
       end
 
       total == result
@@ -24,6 +23,21 @@ end
 
 # ======================================================================
 
-AdventOfCode2024.run(7, 2) do
+POSSIBLE_OPERATORS_PART2 = %w[+ * ||].freeze
 
+AdventOfCode2024.run(7, 2) do
+  input.map do |result, numbers|
+    result if POSSIBLE_OPERATORS_PART2.repeated_permutation(numbers.size - 1).any? do |operators|
+      total = numbers[0]
+      (numbers.size - 1).times do |i|
+        total = if operators[i] == '||'
+                  [total, numbers[i + 1]].join.to_i
+                else
+                  total.send(operators[i].to_sym, numbers[i + 1])
+                end
+      end
+
+      total == result
+    end
+  end.compact.sum
 end
