3d1aade1009e0825134e17d21415d9a5cb4cecdb
Johnny <johnnydongelmans@gmail.com>
2024-12-08 00:21:36 +0100

    Simplified and sped up version of day 7 part 1

--- day07.rb
+++ day07.rb
@@ -2,18 +2,6 @@ require_relative 'advent_of_code_2024'
 
 inputs = File.readlines('inputs/day07.txt')
 
-example = <<~INPUT
-  190: 10 19
-  3267: 81 40 27
-  83: 17 5
-  156: 15 6
-  7290: 6 8 6 15
-  161011: 16 10 13
-  192: 17 8 14
-  21037: 9 7 18 13
-  292: 11 6 16 20
-INPUT
-
 POSSIBLE_OPERATORS = %i[+ *].freeze
 
 AdventOfCode2024.run(7, 1) do
@@ -24,10 +12,9 @@ AdventOfCode2024.run(7, 1) do
 
   input.map do |result, numbers|
     result if POSSIBLE_OPERATORS.repeated_permutation(numbers.size - 1).any? do |operators|
-      equation = numbers.zip(operators).flatten.compact
-      total = equation[0]
-      (1...equation.length).step(2).each do |i|
-        total = total.send(equation[i], equation[i + 1])
+      total = numbers[0]
+      (numbers.size - 1).times do |i|
+        total = total.send(operators[i], numbers[i+1])
       end
 
       total == result
