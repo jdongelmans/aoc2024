fff0c7e7182a138302fefd03ef3e0694f0f386c0
Johnny <johnnydongelmans@gmail.com>
2024-12-08 13:12:01 +0100

    Add a recursive variant to speed it up to ~1 second

--- day07.rb
+++ day07.rb
@@ -25,9 +25,35 @@ def result_possible?(result, numbers, include_concatenation: false)
   end
 end
 
+def recursive_result_possible?(result, numbers, index = 0, total = numbers[0], include_concatenation: false)
+  return total == result if index == numbers.size - 1
+
+  next_index = index + 1
+  next_number = numbers[next_index]
+
+  if (total + next_number) <= result
+    return true if recursive_result_possible?(result, numbers, next_index, total + next_number, include_concatenation: include_concatenation)
+  end
+
+  if (total * next_number) <= result
+    return true if recursive_result_possible?(result, numbers, next_index, total * next_number, include_concatenation: include_concatenation)
+  end
+
+  if include_concatenation
+    new_total = total.concat(next_number)
+
+    if new_total <= result
+      return true if recursive_result_possible?(result, numbers, next_index, new_total, include_concatenation: include_concatenation)
+    end
+  end
+
+  false
+end
+
 AdventOfCode2024.run(7, 1) do
   input.sum do |result, numbers|
-    result_possible?(result, numbers) ? result : 0
+    # result_possible?(result, numbers) ? result : 0
+    recursive_result_possible?(result, numbers) ? result : 0
   end
 end
 
@@ -35,6 +61,7 @@ end
 
 AdventOfCode2024.run(7, 2) do
   input.sum do |result, numbers|
-    result_possible?(result, numbers, include_concatenation: true) ? result : 0
+    # result_possible?(result, numbers, include_concatenation: true) ? result : 0
+    recursive_result_possible?(result, numbers, include_concatenation: true) ? result : 0
   end
 end
